3a05018efb7eae0d1063c4f61fe5036c85bee8f0
Ronan <ronan.delacruz@sdca.edu.ph>
2024-09-15 19:03:19 +0800

    updated version

--- registration.php
+++ registration.php
@@ -4,131 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration</title>
-
-    <style>
-        * {
-          padding: 0;
-          margin: 0;  
-        }
-
-        body {
-            background-color: #f8f7ff;
-            height: 100%;
-            padding-block: 5%;
-            
-        }
-
-        main {
-            margin: auto;
-            height: 30rem;
-            width: 60%; 
-            background-color: #b8b8ff;
-            border-radius: 1rem;
-            padding-block: 2rem;
-        }
-
-        h1 {
-            text-align: center; 
-            color: #4a34c8; 
-            padding-block: 2rem;
-            font-size: 2.5rem;
-        }
-
-        .form-title {
-            margin: auto;
-        }
-        
-        input[type="radio"]{
-            color: white;
-        }
-
-        .flex-field {
-            display: grid;
-            grid-template-columns: 25% 75%;
-            justify-content: flex-end;
-            align-items: center;
-            gap: 10px;
-            padding-inline: 1.3rem;
-            margin-bottom: 2rem;
-            
-        }
-
-        .grid-input {
-            flex-grow: 1;
-            font-size: 12px;
-            padding-block: 6px;
-            padding-inline: 4px;
-            box-sizing: border-box;
-            border-color: beige;
-            background-color: transparent;
-            border-color: transparent transparent black transparent;
-            
-
-        }
-
-        .grid-label {
-            text-align: right;
-            
-        }
-
-        .grid-select {
-            font-size: 12px;
-            padding-block: 6px 4px;
-            line-height: 4px;    
-        }
-
-        .checkbox-form {
-            padding-inline: 4rem;
-            margin-bottom: 2.5rem;
-        }
-        
-        .form-button {
-            background-color: #f8f7ff;
-            color: #9381ff;
-            border: 0px;
-            border-radius: 4px;
-            padding: 1rem 2.4rem
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
+    <main>
+        <h1>Registration</h1>
+        <form action="submit.php" method="post">
+            <input type="text" name="first_name" placeholder="First Name" required>
+            <input type="text" name="last_name" placeholder="Last Name" required>
+            <input type="email" name="user_email" placeholder="Email" required>
+            <input type="password" name="user_password" placeholder="Password" required>
 
+            <?php $currentDate = date('Y-m-d'); ?>
+            <input type="date" name="date" required min="<?php echo $currentDate; ?>">
 
-    <main> 
+            <div class="checkbox-group">
+                <input type="checkbox" name="checkbox" id="java" value="Java">
+                <label for="java">Java</label>
+                <input type="checkbox" name="checkbox" id="python" value="Python">
+                <label for="python">Python</label>
+                <input type="checkbox" name="checkbox" id="dotnet" value=".NET">
+                <label for="dotnet">.NET</label>
+            </div>
 
-        <form action="submit.php" method = "post" > 
-        <input type="text" name="first_name" placeholder="First Name" id="" class="grid-input" required>
-        <input type="text" name="last_name" placeholder="Last Name" id="" class="grid-input" required>
-        <input type="email" name="user_email" placeholder="Email" id="" class="grid-input" required>
-        <input type="password" name="user_password" placeholder="Password" id="" class="grid-input" required>
+            <div class="radio-group">
+                <input type="radio" name="year-level" id="year1" value="1" required>
+                <label for="year1">1</label>
+                <input type="radio" name="year-level" id="year2" value="2" required>
+                <label for="year2">2</label>
+                <input type="radio" name="year-level" id="year3" value="3" required>
+                <label for="year3">3</label>
+                <input type="radio" name="year-level" id="year4" value="4" required>
+                <label for="year4">4</label>
+            </div>
 
-        <?php
-
-         $currentDate = date('Y-m-d');
-        ?>
-
-        <input type="date" name="date" required min="<?php echo $currentDate; ?>">
-
-        <input type="reset" value = "Reset Button">
-
-        <input type="checkbox" name="checkbox" id="" value=""><label for="checkbox"> Java </label>
-        <input type="checkbox" name="checkbox" id="" value=""><label for="checkbox"> Phyton </label>
-        <input type="checkbox" name="checkbox" id="" value=""><label for="checkbox"> .NET </label>
-
-        <input type="radio" name="year-level" id="" value="1" required><label for="year-level"> 1 </label>
-        <input type="radio" name="year-level" id="" value="2" required><label for="year-level"> 2 </label>
-        <input type="radio" name="year-level" id="" value="3" required><label for="year-level"> 3 </label>
-       
-
-
-
-
-        <input type="submit" value="Click Me"> 
-
+            <input type="submit" value="Click Me">
+            <input type="reset" value="Reset">
         </form>
     </main>
-    
 </body>
-
-</html> 
+</html>
